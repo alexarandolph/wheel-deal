ec249e61f636333df134fbf83013279542f96f9e
Alexa Randolph <alexarand7@gmail.com>
2023-01-27 10:33:08 -0500

    updating

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import technician_detail, list_technicians, list_services, service_detail
 
 urlpatterns = [
-    path("automobiles/<int:auto_vo_id>/services/", list_services,name="list_services"),
+    path("services/", list_services,name="list_services"),
     path("services/", list_services, name="create_service"),
     path("services/<int:id>/", service_detail, name="service_detail"),
     path("technicians/", list_technicians, name="list_technicians"),
